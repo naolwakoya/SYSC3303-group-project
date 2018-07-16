025f0f2d429b413aacc6ed6a9fa28658ee3b6d83
Melakusemaw <melakusemaw@cmail.carleton.ca>
2018-07-16 18:14:49 -0400

    Update TftpServer.java

--- src/server/TftpServer.java
+++ src/server/TftpServer.java
@@ -1,10 +1,10 @@
-package server;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
 import java.net.SocketTimeoutException;
+
 public class TftpServer {
 
 
